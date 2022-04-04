f117a65f686ee98d426876222de465bb12826884
Benjamin Wasilewski <benwasilewski@gmail.com>
2022-04-04 00:40:09 -0500

    get refresh and clear sources

--- src/controllers/source.controller.js
+++ src/controllers/source.controller.js
@@ -2,25 +2,44 @@ const catchAsync = require('../utils/catchAsync');
 const Source = require('../models/source.model')
 const fetch = require('node-fetch');
 
-const domain = `https://newsdata.io/api/1/sources?apikey=${process.env.NEWSDATA_KEY}&language=en&country=us`
+const domain = 
+	`https://newsdata.io/api/1/sources?apikey=${process.env.NEWSDATA_KEY}`
+	+ `&language=en`
+	+ `&country=us`
 
-const getSources = catchAsync(async (req, res) => {
-	const response = await Source.find({})
-	res.send(response)
-})
+const getSources = 
+	catchAsync(async (req, res, next) => {
+		try {
+			const response = await Source.find({})
+			res.send(response)
+		} catch (err) {
+			console.log(err)
+			next(err)
+		}
+	})
 
-const refreshSources = catchAsync(async (req, res) => {
-	const response = await fetch(domain)
-	const data = await response.json()
-	await Source.remove({})
-	const saveResponse = await Source.insertMany(data.results)
-	res.send(saveResponse)
-})
+const refreshSources = 
+	catchAsync(async (req, res, next) => {
+		try {
+			const response = await fetch(domain)
+			const data = await response.json()
+			res.send(data)
+		} catch (err) {
+			console.log(err)
+			next(err)
+		}
+	})
 
-const clearSources = catchAsync(async (req, res) => {
-	const response = await Source.remove({})
-	res.send(response)
-})
+const clearSources = 
+	catchAsync(async (req, res, next) => {
+		try {
+			const response = await Source.deleteMany({})
+			res.send(response)
+		} catch (err) {
+			console.log(err)
+			next(err)
+		}
+	})
 
 module.exports = {
 	getSources,
